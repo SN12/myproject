11958f7ccd45e4226b9a6a7f3a0af49d7c95b0f6
SN <>
2013-07-11 11:43:07 +0400

    Change configuration files

--- conf/vars.php
+++ conf/vars.php
@@ -1,10 +1,6 @@
 <?php
 
 /** Configuration Variables **/
-//db conf
-define ('DEVELOPMENT_ENVIRONMENT',true);
-
-//root folder conf
 define('ROOTFOLDER','/home/u205758343/public_html');
 define('DOMAIN','http://monitis.vv.si/');
 ?>
